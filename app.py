3fa9dc231eb931422bb5c1bf143969482a687642
Minter27 <mohammad.alshbool2002@gmail.com>
2019-01-13 01:05:52 +0200

    added helpers

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
 
+from helpers import login_required
+
 from datetime import datetime
 from random import randint
 from sqlite3 import connect
@@ -38,22 +40,31 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 @app.route("/")
+@login_required
 def index():
   return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-  username = request.form.get("username")
-  password = request.form.get("password")
+  # Forget any user_id
+  session.clear()
 
-  query = db.execute("SELECT * FROM users WHERE username = (?)", [username])
+  # User reached route via POST (as by submitting a form via POST)
+  if request.method == "POST":
 
-  if (not check_password_hash(query[2], password)):
-    flash("Passowrd and/or username aren't correct")
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    query = db.execute("SELECT * FROM users WHERE username = (?)", [username])
+
+    if (not check_password_hash(query[2], password)):
+      flash("Passowrd and/or username aren't correct")
+      return redirect("/")
+
+      # Remember which user has logged in
+    session["user_id"] = query[0]
+
+    # Redirect user to home page
     return redirect("/")
-
-    # Remember which user has logged in
-  session["user_id"] = query[0]
-
-  # Redirect user to home page
-  return redirect("/")
+  else:
+    return render_template("login.html")
