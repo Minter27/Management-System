3a8a3af7156298623e249d1ab165f57707cb5ccf
Mohammad Shbool <shbool@Mohammads-MacBook-Pro.local>
2019-02-22 16:11:04 +0200

    cleared everything and made it ready

--- launchServer.py
+++ launchServer.py
@@ -7,11 +7,11 @@ async def main():
   await asyncio.sleep(3)
   webbrowser.open("http://127.0.0.1:5000")
 
-os.system("cat app.py")
 asyncio.run(main())
 
 # This is on UNIX systems only
 # If using something other than a UNIX-based system,
-# Uncomment the next line and remove the last line
+# Uncomment the next two lines and remove the last line
+# os.system("set FLASK_APP=app.py")
 # os.system("flask run")
 os.system("gunicorn -w 4 -b 127.0.0.1:5000 app:app")
